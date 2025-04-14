41e4a7e3f8dca6413da5b9b18ea8ac2157ed982b
andmohiko <andmohiko@gmail.com>
2025-04-14 22:45:59 +0900

    fix: use callback

--- apps/web/src/hooks/useToast.tsx
+++ apps/web/src/hooks/useToast.tsx
@@ -1,9 +1,10 @@
 import { showNotification } from '@mantine/notifications'
+import { useCallback } from 'react'
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 import { BiErrorCircle } from 'react-icons/bi'
 
 export const useToast = () => {
-  const showSuccessToast = (title: string, message?: string) => {
+  const showSuccessToast = useCallback((title: string, message?: string) => {
     if (typeof window === 'undefined') {
       return
     }
@@ -15,9 +16,9 @@ export const useToast = () => {
       autoClose: 4000,
       color: 'green',
     })
-  }
+  }, [])
 
-  const showErrorToast = (title: string, message?: string) => {
+  const showErrorToast = useCallback((title: string, message?: string) => {
     if (typeof window === 'undefined') {
       return
     }
@@ -29,7 +30,7 @@ export const useToast = () => {
       autoClose: 8000,
       color: 'red',
     })
-  }
+  }, [])
 
   return {
     showSuccessToast,
