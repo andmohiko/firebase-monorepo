e315f91245af538cf4a7cf6960bb46dcbe7c4318
andmohiko <andmohiko@gmail.com>
2026-04-07 00:07:54 +0900

    chore: update tanstack deps

--- apps/tanstack-start/src/routes/index.tsx
+++ apps/tanstack-start/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Button } from '#/components/ui/button'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({ component: App })
 
